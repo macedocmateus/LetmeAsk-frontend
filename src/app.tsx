8132d640d1f7ec788da3cce1db034827666e7861
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-15 13:50:52 -0300

    add install and config shadcn ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
+import { Button } from './components/ui/button'
 import './index.css'
 
 export function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <Button>Hello world</Button>
+      <Button variant='secondary'>Hello world</Button>
+      <Button variant='outline'>Hello world</Button>
+      <Button variant='ghost'>Hello world</Button>
+      <Button variant='destructive'>Hello world</Button>
+    </div>
   )
 }
