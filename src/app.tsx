723ef326d58e3ef39657a77e0369bb6ff23d1a3b
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-15 15:14:50 -0300

    add tanstack/react-query to communicate with backend

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<CreateRoom/>}/>
-        <Route path='/room' element={<Room/>}/>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom/>} index/>
+          <Route element={<Room/>} path='/room/:roomId'/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,4 +1,18 @@
+import {useParams, Navigate} from 'react-router-dom'
+
+type RoomParams = {
+    roomId: string
+}
+
 export function Room() {
+    const params  = useParams<RoomParams>()
+
+    params.roomId
+
+    if (!params.roomId) {
+        return <Navigate replace to='/'/>
+    }
+    
     return (
         <div>Room Details</div>
     )
