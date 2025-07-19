295a6ce8eb36c9d41c30c0a8dc8428576e80bf98
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-19 13:31:09 -0300

    fix bug from questionCount for questionsCount

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,33 +1,73 @@
+import { Badge } from '@/components/ui/badge'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
-import {Link} from 'react-router-dom'
+import { ArrowRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
-type GetRoomsAPIResponse =  Array<{
-    id: string,
-    name: string
+type GetRoomsAPIResponse = Array<{
+  id: string
+  name: string
+  questionsCount: number
+  createdAt: string
 }>
 
 export function CreateRoom() {
-    const {data, isLoading} = useQuery({
-        queryKey: ['get-rooms'],
-        queryFn: async() => {
-            const response = await fetch('http://localhost:3333/rooms')
-            const result: GetRoomsAPIResponse = await response.json()
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: GetRoomsAPIResponse = await response.json()
 
-            return result
-        }
-    })
-    
-    return (
-        <div>    
-            {isLoading && <p>Carregando...</p>}
-            <div className='flex flex-col gap-1'>
-                {data?.map(room => {
-                    return (
-                        <Link key={room.id} to={`/room/${room.id}`}>{room.name}
-                        </Link>
-                    )
-                })}
-            </div>
+      return result
+    },
+  })
+
+  return (
+    <div className="min-h-screen px-8 py-4">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div />
+
+          <Card>
+            <CardHeader>
+              <CardTitle>Salas recentes</CardTitle>
+              <CardDescription>
+                Acesso rápido as salas criadas recentemente
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+              {data?.map((room) => {
+                return (
+                  <Link
+                    key={room.id}
+                    to={`/rooms/${room.id}`}
+                    className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent"
+                  >
+                    <div className="flex flex-1  flex-col gap-1">
+                      <h3 className="font-medium">{room.name}</h3>
+
+                      <div className="flex items-center gap-2">
+                        <Badge>{room.questionsCount} perguntas</Badge>
+                      </div>
+                    </div>
+
+                    <span className="flex items-center gap-1 text-sm">
+                      Entrar
+                      <ArrowRight className="size-3" />
+                    </span>
+                  </Link>
+                )
+              })}
+            </CardContent>
+          </Card>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
