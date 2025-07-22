69daede7e1a8cdec4e1864c251e7620071e98259
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-21 22:37:13 -0300

    add refactoring room list

---
+++ src/components/room-list.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Link } from "react-router-dom"
+import { Badge } from "./ui/badge"
+import { dayjs } from "@/lib/dayjs"
+import { ArrowRight } from "lucide-react"
+import { useRooms } from "@/http/use-rooms"
+
+export function RoomList() {
+    const {data, isLoading} = useRooms()
+    
+    return (
+        <Card>
+            <CardHeader>
+              <CardTitle>Salas recentes</CardTitle>
+              <CardDescription>
+                Acesso rápido as salas criadas recentemente
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+              {isLoading && <p className='text-muted-foreground text-sm'>Carregando salas...</p>}
+              
+              {data?.map((room) => {
+                return (
+                  <Link
+                    key={room.id}
+                    to={`/rooms/${room.id}`}
+                    className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent/50"
+                  >
+                    <div className="flex flex-1  flex-col gap-1">
+                      <h3 className="font-medium">{room.name}</h3>
+
+                      <div className="flex items-center gap-2">
+                        <Badge className="text-xs" variant="secondary">{dayjs(room.createdAt).toNow()}</Badge>
+                        <Badge className="text-xs" variant="secondary">{room.questionsCount} perguntas</Badge>
+                      </div>
+                    </div>
+
+                    <span className="flex items-center gap-1 text-sm">
+                      Entrar
+                      <ArrowRight className="size-3" />
+                    </span>
+                  </Link>
+                )
+              })}
+            </CardContent>
+          </Card>)
+    
+}
--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,75 +1,12 @@
-import { Badge } from '@/components/ui/badge'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { dayjs } from '@/lib/dayjs'
-import { useQuery } from '@tanstack/react-query'
-import { ArrowRight } from 'lucide-react'
-import { Link } from 'react-router-dom'
-
-type GetRoomsAPIResponse = Array<{
-  id: string
-  name: string
-  questionsCount: number
-  createdAt: string
-}>
+import { RoomList } from '@/components/room-list'
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
-      const result: GetRoomsAPIResponse = await response.json()
-
-      return result
-    },
-  })
-
   return (
     <div className="min-h-screen px-8 py-4">
       <div className="mx-auto max-w-4xl">
         <div className="grid grid-cols-2 items-start gap-8">
           <div />
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Salas recentes</CardTitle>
-              <CardDescription>
-                Acesso rápido as salas criadas recentemente
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-3">
-              {isLoading && <p className='text-muted-foreground text-sm'>Carregando salas...</p>}
-              
-              {data?.map((room) => {
-                return (
-                  <Link
-                    key={room.id}
-                    to={`/rooms/${room.id}`}
-                    className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent/50"
-                  >
-                    <div className="flex flex-1  flex-col gap-1">
-                      <h3 className="font-medium">{room.name}</h3>
-
-                      <div className="flex items-center gap-2">
-                        <Badge className="text-xs" variant="secondary">{dayjs(room.createdAt).toNow()}</Badge>
-                        <Badge className="text-xs" variant="secondary">{room.questionsCount} perguntas</Badge>
-                      </div>
-                    </div>
-
-                    <span className="flex items-center gap-1 text-sm">
-                      Entrar
-                      <ArrowRight className="size-3" />
-                    </span>
-                  </Link>
-                )
-              })}
-            </CardContent>
-          </Card>
+          <RoomList/>
         </div>
       </div>
     </div>
